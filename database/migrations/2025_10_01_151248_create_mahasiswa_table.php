60b72a0ad0d55b8f2e1b1ca8e8e890297986b92e
saya-basir <ahmad.basyir@mhs.politala.ac.id>
2025-10-14 19:45:06 +0800

    ok

--- database/migrations/2025_10_01_151248_create_mahasiswa_table.php
+++ database/migrations/2025_10_01_151248_create_mahasiswa_table.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,12 +11,13 @@ return new class extends Migration
         Schema::create('mahasiswa', function (Blueprint $table) {
             $table->id();
             $table->string('namaLengkap', 100);
-            $table->string('nim', 20)->unique(); // nim/nip harus unik
+            $table->string('nim', 20)->unique();
             $table->string('email', 100)->unique();
             $table->string('username', 50)->unique();
-            $table->string('password'); // Laravel otomatis mengurus panjangnya (255)
+            $table->string('password');
             $table->enum('role', ['mahasiswa', 'dosen', 'admin', 'staff'])->default('mahasiswa');
-            $table->timestamps(); // membuat created_at dan updated_at
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
